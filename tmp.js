5dad43d9879fe2dd285dcc999401244589a7db19
Daniel Olsson <kakomitslie@gmail.com>
2022-11-20 12:01:53 +0100

    steps

--- tmp.js
+++ tmp.js
@@ -1,21 +1,21 @@
 (function () {
     var App = function () {
 
-        let args = [].slice.call(arguments),
+        let args = [].slice.call(arguments), // 5, 7
             callback = args.pop(),
             modules = (args[0] && typeof args[0] === 'string') ? args : args[0],
             copyModules = {},
             i;
 
         if (!(this instanceof App)) {
-            return new App(modules, callback);
+            return new App(modules, callback); // 6
         }
 
         function addModuleToCopyObject(module) {
-            copyModules[module] = {};
+            copyModules[module] = {}; // 9
 
             function copyModule(parent, child) {
-                for (i in parent) {
+                for (i in parent) { // 11
                     if (parent.hasOwnProperty(i)) {
                         child[i] = parent[i]
                     }
@@ -24,27 +24,35 @@
                 return child;
             }
 
-            copyModule(App.modules[module], copyModules[module]);
+            copyModule(App.modules[module], copyModules[module]); // 10
         }
 
-        modules.forEach(addModuleToCopyObject);
-        callback(copyModules);
+        modules.forEach(addModuleToCopyObject); // 8
+        callback(copyModules); // 12
     };
 
     if (typeof window.App === 'undefined') {
-        window.App = App;
+        window.App = App; // 1
     }
 })();
 
-App.modules = {};
+App.modules = {}; // 2
 
-App.modules.module1 = (function(){
+App.modules.module1 = (function(){ // 3
 
     function _doSmth(container) {
-        container.innerText = 'It works';
+        container.innerText = 'It works'; // 14
     }
 
     return {
         doSmth: _doSmth,
     }
 })();
+
+// Usage
+(function(){
+    App(['module1'], function (Modules) {
+        let container = document.getElementById('container'); // 13
+        Modules['module1'].doSmth(container);
+    });
+})(); // 4
